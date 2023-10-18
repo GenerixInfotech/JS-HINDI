a196612de9a2fc3174d939982d9fa5db31e18749
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-10-18 10:26:01 +0000

    datatytpe

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,19 @@
 "use strict"; // treat all JS Code as newer version
 
-alert(3 + 3); // We are using Node JS not browser
+// alert(3 + 3); // We are using Node JS not browser
+
+let name = "Mehul"
+let age = 9
+let isLoggedIn = true
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value hai
+// undefined => not with value
+// symbol => unique
+
+
+// object 
+console.log(typeof age);
