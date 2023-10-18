bb84bc4a4f79170e3f0912a12630e0ff0ef41ae4
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-10-18 09:45:29 +0000

    new changes

---
+++ 01_basics/01_varibales.js
@@ -0,0 +1,15 @@
+const accountId = 12121;
+let accountEmail = "manish@gmail.com";
+var accountPassword = "123456";
+accountCity = "Jamshedpur";
+
+let accountState;
+
+accountEmail = "mm@gmail.com";
+accountPassword = "33333";
+accountCity = "Kolkata";
+
+// accountId = 2;
+
+console.log(accountId);
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,3 @@
+"use strict"; // treat all JS Code as newer version
+
+alert(3 + 3); // We are using Node JS not browser
