49cd398f9695025e9300b1aa4c7fc4494e12a9dd
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-10-25 10:57:43 +0000

    new

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -23,5 +23,32 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************* Opration ********** 
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%2);
+// console.log(2/2);
+
+let str1 = "Hello"
+let str2 = " Manish"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(2 + 3 + "7");
+
+console.log(+true)
+console.log(+"");
