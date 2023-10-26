65dfc39847fda0972a0bc5c7e1f73c9a541f9d03
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-10-26 08:04:09 +0000

    datasummary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,12 @@
+//  +++++++ Primitive +++++++++++
+// Type of Primitive 7 Type: String, Number, Boolean, Null, Symbol, Undefined, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let useEmail;
+// ++++++++++++ Reference (Non Primitive) +++++++++++++
+// Type of Non Primitive : Array, Objects, Functions
+
