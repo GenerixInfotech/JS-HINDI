99b4962d8be2781dd18ba481ff9567b59265b363
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-10-30 07:41:30 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,8 @@
+const name = "Manish"
+const repoCount = 30
+
+console.log(name + repoCount + " value");
+
+console.log(`My name is ${name} and my total repogotry is ${repoCount}`);
+
+const gameName = new String('GameZone')
