b5b6fb269bb987ef955d93038f29404a40a35aa2
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-10-30 10:20:20 +0000

    change string

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,13 +1,32 @@
 const name = "Manish"
 const repoCount = 30
 
-console.log(name + repoCount + " value");
+// console.log(name + repoCount + " value");
 
-console.log(`My name is ${name} and my total repogotry is ${repoCount}`);
+// console.log(`My name is ${name} and my total repogotry is ${repoCount}`);
 
-const gameName = new String('GameZone')
+const gameName = new String('Game-Zone')
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.charAt);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('m'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "    Manish       "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://www.manish.com/manish%20mahapatra"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('rashmi'))
+
+console.log(gameName.split('-'));
