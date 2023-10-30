eef709290dd4a5f7484080eef0c9239b822ef179
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-10-30 08:09:54 +0000

    stringchange

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -6,3 +6,8 @@ console.log(name + repoCount + " value");
 console.log(`My name is ${name} and my total repogotry is ${repoCount}`);
 
 const gameName = new String('GameZone')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.charAt);
