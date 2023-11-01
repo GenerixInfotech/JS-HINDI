c22b71e5faa9ebbf13b50d359f8e3b4a0e44b959
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-01 08:56:30 +0000

    OBJECT

---
+++ 02_basics/03_object.js
@@ -0,0 +1,12 @@
+// singleton
+//Object.create
+// object literals
+
+const jsUser ={
+    name:"manish",
+    age:42,
+    location: "jamshedpur",
+    email: "manish@google.com",
+    isloggedIn: false,
+    lastLogin: ["Monday", "Saturday"]
+}
