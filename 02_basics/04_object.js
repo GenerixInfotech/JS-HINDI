9394d6b365f3dd1d092a0de29efe47d4160e25a0
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-03 09:28:25 +0000

    cg

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -17,3 +17,14 @@ const regularUser = {
         }
     }
 }
+// console.log(regularUser.fullName.userFullName.fistName);
+
+const obj01 ={1:"a", 2:"b"}
+const obj02 ={3:"c", 4:"d"}
+const obj04 ={5:"e", 6:"f"}
+
+// const obj03 = {obj01, obj02}
+// const obj03 = Object.assign({}, obj01, obj02, obj04)
+
+const obj03 = {...obj01, ...obj02} //Spread Method like Array [....xyz, ...pqr]
+console.log(obj03);
