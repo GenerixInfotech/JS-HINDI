2c708971ae98638aaaac0ae5b4bbf5f3cf235d24
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-03 09:11:28 +0000

    object04

---
+++ 02_basics/04_object.js
@@ -0,0 +1,19 @@
+// const tinderUser = new Object()
+
+const tinderUser = {}
+
+tinderUser.id = "123"
+tinderUser.email = "m@gmail.com"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"someuser@gmail.com",
+    fullName:{
+        userFullName:{
+            fistName:"Mehul",
+            lastName:"Mahapatra"
+        }
+    }
+}
