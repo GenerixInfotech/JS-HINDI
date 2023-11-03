255b60673ad77a9568678a0079dc993e6394aa4f
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-03 09:44:03 +0000

    finalchange

--- 02_basics/04_object.js
+++ 02_basics/04_object.js
@@ -27,4 +27,27 @@ const obj04 ={5:"e", 6:"f"}
 // const obj03 = Object.assign({}, obj01, obj02, obj04)
 
 const obj03 = {...obj01, ...obj02} //Spread Method like Array [....xyz, ...pqr]
-console.log(obj03);
+// console.log(obj03);
+
+const users = [
+    {
+        id:"1",
+        email:"xyz@gmail.com"
+    },
+    {
+        id:"2",
+        email:"pqr@gmail.com"
+    },
+    {
+        id:"3",
+        email:"mno@gmail.com"
+    }
+]
+// console.log(users[0].email);
+// console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('isLoggedOut'));
