7859b1942ce04a2d8a283e22c0b98f69ce3a3ad4
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-03 07:28:33 +0000

    newobjss

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -1,15 +1,24 @@
 // singleton
 //Object.create
 // object literals
+const mySmy = Symbol("mySmy01")
 
 const jsUser ={
     name:"manish",
+    "full name":"manish mahapatra",
+    [mySmy]:"newSymbol",
     age:42,
     location: "jamshedpur",
     email: "manish@google.com",
     isloggedIn: false,
     lastLogin: ["Monday", "Saturday"]
+
 }
 
 console.log(jsUser.email);
 console.log(jsUser["email"]);
+console.log(jsUser["full name"]);
+console.log(jsUser[mySmy]);
+
+Object.freeze(jsUser)
+console.log(jsUser);
