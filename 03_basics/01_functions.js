b0cef9867aa6ba49e2fc70215d0d02f46a9ac3db
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-14 09:17:46 +0000

    function complete

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,4 +30,31 @@ function loginUserMessage(username = "Mehul"){
     return `${username} just logged in`
 }
 // console.log(loginUserMessage("manish"))
-console.log(loginUserMessage("Manish"));
+// console.log(loginUserMessage("Manish"));
+
+function calculateCartPrice(val1, val2, ...num1) {
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500, 700));
+
+const user = {
+    username: "manish",
+    prices: 299
+}
+function handelObject(anyObject) {
+    console.log(`User name is ${anyObject.username} and is price ${anyObject.price}`);
+}
+// handelObject(user)
+handelObject({
+    username:"Mehul",
+    price:300
+})
+
+
+const newArray = [200, 500, 600, 800]
+
+function returnSecondValue(getArray) {
+    return getArray[1]
+}
+// console.log(returnSecondValue(newArray));
+console.log(returnSecondValue([200,5000,600,800]));
