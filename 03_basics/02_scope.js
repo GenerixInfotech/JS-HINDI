4a3341261afcd159cdafe3de46d4ad296ad1749e
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-14 12:06:06 +0000

    scope com

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -3,9 +3,22 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one() {
+    const username = "manish"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+one()
