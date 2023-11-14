e5266e3933bc9defcd43f747e064cb4de34e40d5
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-14 06:47:45 +0000

    function today

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,7 +8,26 @@ function sayName(){
 }
 // sayName()
 
+// function addTwoNumbers(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// addTwoNumbers("6", 5)
+
 function addTwoNumbers(num1, num2) {
-    console.log(num1 + num2);
+    // let result = num1 + num2;
+    // return result;
+    return num1 + num2
 }
-addTwoNumbers("6", 5)
+const result = addTwoNumbers(5, 3)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "Mehul"){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("manish"))
+console.log(loginUserMessage("Manish"));
