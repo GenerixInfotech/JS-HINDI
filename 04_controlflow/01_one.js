d3bfca395d60c59068f90330d08d01cdfdc01c16
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-24 10:01:54 +0000

    controlflow_final

--- 04_controlflow/01_one.js
+++ 04_controlflow/01_one.js
@@ -19,3 +19,31 @@
 // }
 // console.log(`User power: ${power}`);
 
+// const balance = 1000
+
+// if(balance > 500) console.log("test"), console.log("test2");
+
+// if(balance < 500){
+//     console.log("less then 500");
+// }else if(balance < 750){
+//     console.log("less then 750");
+// }else if(balance < 900){
+//     console.log("less then 900");
+// }else {
+//     console.log("less then 1200");
+// }
+
+
+const userLoggedIn = true;
+const debitCard = true;
+
+const loggedInFromGoogle = true;
+const loggedInFromEmail = true;
+
+if(userLoggedIn && debitCard && 2==2){
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User Logged In");
+}
