16b441f50b7d7a934475fc34b135ae9f92efd9d6
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-24 07:22:35 +0000

    control_flow

---
+++ 04_controlflow/01_one.js
@@ -0,0 +1,13 @@
+// if
+// const userLoggedIn = true
+// const temperature = 41
+
+// if(temperature === 41){
+//     console.log("Less then 40");
+// }else{
+//     console.log("Grater then 40");
+// }
+// console.log("Excuted");
+
+//<, >, <=, >=, ==, !=
+
