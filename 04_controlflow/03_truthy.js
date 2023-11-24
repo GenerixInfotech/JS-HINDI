5868629208f293e305958c4c85f0551cdca51719
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-24 11:07:08 +0000

    trutly final

---
+++ 04_controlflow/03_truthy.js
@@ -0,0 +1,39 @@
+const userEmail = []
+
+if(userEmail){
+    console.log("Got user Email");
+}else{
+    console.log("Dont have user Email");
+}
+
+//++++++++++++ falsy value are +++++++++++++++++++++++
+// false, 0, -0, Bigint 0n, "", null, undefined, NaN
+
+//++++++++++++++ Truthy Values are ++++++++++++++++++++
+// [], {}, '0', 'false', " ", function(){}, 
+
+// if (userEmail.length === 0) {
+//     console.log("Array is Empty");
+// }
+
+const emptyObj = {}
+
+if (Object.keys(emptyObj).length === 0) {
+    console.log("Object is Empty");
+}
+
+// Nullish Coalescing Operator (??) : null undefined
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 15
+// val1 = undefined ?? 20
+val1 = null ?? 30 ?? 50
+
+console.log(val1);
+
+// Terniary Operator (?) 
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice >= 80 ? console.log("Leass then 80") : console.log("More then 80");
+
