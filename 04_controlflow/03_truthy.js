1e292de0fc575172efe8fc943119aff33572d05b
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-24 11:46:50 +0000

    final truthy

--- 04_controlflow/03_truthy.js
+++ 04_controlflow/03_truthy.js
@@ -1,10 +1,10 @@
-const userEmail = []
+// const userEmail = []
 
-if(userEmail){
-    console.log("Got user Email");
-}else{
-    console.log("Dont have user Email");
-}
+// if(userEmail){
+//     console.log("Got user Email");
+// }else{
+//     console.log("Dont have user Email");
+// }
 
 //++++++++++++ falsy value are +++++++++++++++++++++++
 // false, 0, -0, Bigint 0n, "", null, undefined, NaN
@@ -16,24 +16,24 @@ if(userEmail){
 //     console.log("Array is Empty");
 // }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is Empty");
-}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is Empty");
+// }
 
 // Nullish Coalescing Operator (??) : null undefined
 let val1;
 // val1 = 5 ?? 10
 // val1 = null ?? 15
 // val1 = undefined ?? 20
-val1 = null ?? 30 ?? 50
+// val1 = null ?? 30 ?? 50
 
-console.log(val1);
+// console.log(val1);
 
 // Terniary Operator (?) 
 // condition ? true : false
 
 const iceTeaPrice = 100
-iceTeaPrice >= 80 ? console.log("Leass then 80") : console.log("More then 80");
+iceTeaPrice <= 80 ? console.log("Leass then 80") : console.log("More then 80");
 
