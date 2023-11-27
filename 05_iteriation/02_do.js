30cb26b3ddf11c54012800f51abef8bb3591a823
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-27 05:53:30 +0000

    do loop

--- 05_iteriation/02_do.js
+++ 05_iteriation/02_do.js
@@ -11,3 +11,9 @@ while (arr < myArray.length) {
     console.log(`Value is ${myArray[arr]}`);
     arr = arr + 1
 }
+
+let score =1
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
