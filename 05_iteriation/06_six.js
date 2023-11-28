860a71c8b2ba20b1b2005d144f82e2b9220a5763
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-11-28 06:49:09 +0000

    filter_push

---
+++ 05_iteriation/06_six.js
@@ -0,0 +1,28 @@
+// const coding = ["js", "ruby", "java", "python"]
+
+// const values = coding.forEach( (items) => {
+//     console.log(items);
+//     return items
+// })
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNums = myNums.filter( (nums) => nums > 4)
+// console.log(newNums);
+
+// const newNums = myNums.filter( (nums) => {
+//     return nums > 4   
+// })
+// console.log(newNums);
+
+// const newNums = []
+
+// myNums.forEach((items) => {
+//     if (items > 4) {
+//         newNums.push(items)
+//     }
+// });
+// console.log(newNums);
+
