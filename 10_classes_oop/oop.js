cbe36c53dd5a5aeb978a705800c203f606c8d96d
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2023-12-21 07:04:46 +0000

    final oop

--- 10_classes_oop/oop.js
+++ 10_classes_oop/oop.js
@@ -2,5 +2,30 @@ const user ={
     userName:'Mehul',
     loginCount:8,
     signIn:true,
+
+    getuserDetail: function () {
+        // console.log("Got user details from database");
+        // console.log(`username: ${this.userName}`);
+        console.log(this);
+    }
 }
-console.log(user.userName)
+// console.log(user["userName"])
+// console.log(user.getuserDetail());
+// console.log(this);
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    this.gretting = function () {
+        console.log(`Welcome ${this.username}`);
+    }
+
+    return this
+}
+
+const userOne = new User("Ravi", 18, true)
+const userTwo = new User("Seema", 10, false)
+console.log(userOne.constructor);
+// console.log(userTwo);
