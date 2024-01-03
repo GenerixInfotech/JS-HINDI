edd735bb35a803cd360d94b084383abcd5ed4a12
GenerixInfotech <147803928+GenerixInfotech@users.noreply.github.com>
2024-01-03 08:37:34 +0000

    inheritance_file

---
+++ 10_classes_oop/inheritance.js
@@ -0,0 +1,29 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+    logMe(){
+        console.log(`UserName is ${this.username}`);
+    }
+}
+class Teacher extends User {
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+    addCourse(){
+        console.log(`A new course added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("Priya", "priya@gmail.com", 1234)
+
+chai.addCourse()
+const masalaChai = new User("MasalaChai")
+
+masalaChai.logMe()
+
+// console.log(chai === masalaChai);
+
+console.log(chai instanceof User);
